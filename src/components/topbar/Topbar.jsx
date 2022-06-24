74485010d69f6a416ef2e4e7570f3f0a8cf06511
victormssa <victormssalves@outlook.com>
2022-06-24 04:12:33 -0300

    Commit inicial

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,20 @@
+import "./topbar.scss";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">Intro</a>
+          <a href="#portfolio">Portfolio</a>
+          <a href="#works">Works</a>
+          <a href="#testimonials">Testimonials</a>
+          <a href="#contact">Contact</a>
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
+    </div>
+  )
+}
