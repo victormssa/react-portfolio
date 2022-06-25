11fc29b63876f35ba327aac4dc71026f23e8340c
victormssa <victormssalves@outlook.com>
2022-06-25 11:35:04 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
 import Works from './components/works/Works';
 import Testimonials from './components/testimonials/Testimonials';
+import Menu from './components/menu/Menu';
 import Contact from './components/contact/Contact';
 import { useState } from "react";
 
@@ -13,6 +14,7 @@ function App() {
     
     <div className="app">
      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
      <div className="sections">
       <Intro/>
       <Portfolio/>
